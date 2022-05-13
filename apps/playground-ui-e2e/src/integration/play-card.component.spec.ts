caef17b907c272562bcc81cdda0c94ffa8dc9a61
Aaron Chesley <FlashBanistan66@gmail.com>
2022-05-13 15:06:47 -0600

    remove playground-ui-e2e project

--- apps/playground-ui-e2e/src/integration/play-card.component.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('playground-ui', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=playcardcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('play-card').should('exist');
-  });
-});
