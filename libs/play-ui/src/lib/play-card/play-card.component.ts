b60c3ac3734fc768d842b49fb7c1e35f6fdc2b8b
Aaron Chesley <FlashBanistan66@gmail.com>
2022-05-14 16:03:49 -0600

    setup card structure

--- libs/play-ui/src/lib/play-card/play-card.component.ts
+++ libs/play-ui/src/lib/play-card/play-card.component.ts
@@ -1,9 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostBinding,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'play-card',
   templateUrl: './play-card.component.html',
   styleUrls: ['./play-card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
-export class PlayCardComponent {}
+export class PlayCardComponent {
+  @HostBinding('class') className = 'play-card';
+}
