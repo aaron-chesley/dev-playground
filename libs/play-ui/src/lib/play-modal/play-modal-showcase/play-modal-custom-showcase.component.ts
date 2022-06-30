fde407321a8d601d0c7639c11a1acfaa97dce1b8
Aaron Chesley <FlashBanistan66@gmail.com>
2022-06-29 21:46:37 -0600

    update modal demo

--- libs/play-ui/src/lib/play-modal/play-modal-showcase/play-modal-custom-showcase.component.ts
+++ libs/play-ui/src/lib/play-modal/play-modal-showcase/play-modal-custom-showcase.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { PlayModalService } from '../play-modal.service';
 
 @Component({
   selector: 'play-custom-showcase',
-  template: `<div>
-    <span>Play Custom Showcase Works!</span>
+  template: `<div class="container">
+    <span>Play Custom Showcase Works!</span
+    ><button (click)="openModal()">Open Another Modal</button>
   </div>`,
+  styles: [
+    `
+      .container {
+        height: 400px;
+        width: 200px;
+      }
+    `,
+  ],
 })
-export class PlayCustomShowcaseComponent {}
+export class PlayCustomShowcaseComponent {
+  openModal() {
+    this.playModalService.alert({
+      alertBody: 'Are you sure you want to do that?',
+    });
+  }
+  constructor(private playModalService: PlayModalService) {}
+}
