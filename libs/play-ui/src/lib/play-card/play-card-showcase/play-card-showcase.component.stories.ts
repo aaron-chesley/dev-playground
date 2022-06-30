c95339689741ac7dfc337a51265cb0353a9c8ea3
Aaron Chesley <FlashBanistan66@gmail.com>
2022-06-30 13:59:36 -0600

    simplify card css

--- libs/play-ui/src/lib/play-card/play-card-showcase/play-card-showcase.component.stories.ts
+++ libs/play-ui/src/lib/play-card/play-card-showcase/play-card-showcase.component.stories.ts
@@ -22,5 +22,5 @@ export const Showcase = Template.bind({});
 Showcase.args = {
   showHeader: true,
   showFooter: true,
-  cardHeight: 498,
+  cardHeight: 374,
 };
--- libs/play-ui/src/lib/play-card/play-card/play-card.component.ts
+++ libs/play-ui/src/lib/play-card/play-card/play-card.component.ts
@@ -1,12 +1,9 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ContentChild,
   HostBinding,
   ViewEncapsulation,
 } from '@angular/core';
-import { PlayCardFooterComponent } from '../play-card-footer/play-card-footer.component';
-import { PlayCardHeaderComponent } from '../play-card-header/play-card-header.component';
 
 @Component({
   selector: 'play-card',
@@ -17,16 +14,4 @@ import { PlayCardHeaderComponent } from '../play-card-header/play-card-header.co
 })
 export class PlayCardComponent {
   @HostBinding('class.play-card') playCardClass = 'play-card';
-  @HostBinding('class.play-card-has-header') playCardHasHeader = '';
-  @HostBinding('class.play-card-has-footer') playCardHasFooter = '';
-
-  @ContentChild(PlayCardHeaderComponent)
-  set playCardHeaderComponent(value: PlayCardHeaderComponent) {
-    this.playCardHasHeader = value ? 'play-card-has-header' : '';
-  }
-
-  @ContentChild(PlayCardFooterComponent)
-  set playCardFooterComponent(value: PlayCardFooterComponent) {
-    this.playCardHasFooter = value ? 'play-card-has-footer' : '';
-  }
 }
