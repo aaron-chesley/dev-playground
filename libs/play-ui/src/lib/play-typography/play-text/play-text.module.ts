5840dc0ca4f224e1f81385047580d704dc014c34
Aaron Chesley <FlashBanistan66@gmail.com>
2022-07-10 21:11:54 -0600

    add play-text component

---
+++ libs/play-ui/src/lib/play-typography/play-text/play-text.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { PlayTextComponent } from './play-text.component';
+
+@NgModule({
+  imports: [CommonModule],
+  exports: [PlayTextComponent],
+  declarations: [PlayTextComponent],
+  providers: [],
+})
+export class PlayTextModule {}
--- libs/play-ui/src/lib/play-typography/showcase/play-typography-showcase.module.ts
+++ libs/play-ui/src/lib/play-typography/showcase/play-typography-showcase.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PlayHeadingModule } from '../play-heading/play-heading.module';
+import { PlayTextModule } from '../play-text/play-text.module';
 
 import { PlayTypographyShowcaseComponent } from './play-typography-showcase.component';
 
 @NgModule({
-  imports: [PlayHeadingModule],
+  imports: [PlayHeadingModule, PlayTextModule],
   exports: [PlayTypographyShowcaseComponent],
   declarations: [PlayTypographyShowcaseComponent],
   providers: [],
