a23d8be10c64e09f98734ea84914325bf319f61e
Aaron Chesley <FlashBanistan66@gmail.com>
2022-08-12 18:29:11 -0600

    create service-worker project

---
+++ apps/service-worker-poc/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dev-playground-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'service-worker-poc';
+}
