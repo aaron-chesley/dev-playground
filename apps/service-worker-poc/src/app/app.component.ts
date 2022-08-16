0ff7310821c361ebe8616baf38aa236e7315c29a
Aaron Chesley <FlashBanistan66@gmail.com>
2022-08-15 21:32:02 -0600

    add boilerplate for notifications

--- apps/service-worker-poc/src/app/app.component.ts
+++ apps/service-worker-poc/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
 
 @Component({
   selector: 'dev-playground-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'service-worker-poc';
+
+  ngOnInit() {
+    this.swPush.notificationClicks.subscribe((res) =>
+      console.log('click:', res)
+    );
+    Notification.requestPermission().then((res) => {
+      navigator.serviceWorker;
+      navigator.serviceWorker.ready.then((registration) => {
+        navigator.serviceWorker;
+        console.log('registration: ', registration);
+        registration.showNotification('Notification With Service Worker', {
+          actions: [
+            {
+              title: 'Answer Call',
+              action: 'ANSWER_CALL',
+            },
+          ],
+        });
+      });
+    });
+  }
+
+  constructor(private swPush: SwPush) {}
 }
