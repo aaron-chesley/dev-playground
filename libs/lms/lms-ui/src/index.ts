94e004ecffab5fad04d384f7f004057234eee127
Aaron Chesley <FlashBanistan66@gmail.com>
2022-08-25 21:28:15 -0600

    update exports

--- libs/lms/lms-ui/src/index.ts
+++ libs/lms/lms-ui/src/index.ts
@@ -1,4 +1,3 @@
 export * from './lib/lms-ui-login/lms-ui-login.component';
-export * from './lib/lms-ui-sidebar/lms-feature-sidebar/lms-ui-sidebar.component';
-export * from './lib/lms-ui-sidebar/lms-feature-sidebar-item/lms-feature-sidebar-item.component';
-export * from './lib/lms-ui-sidebar/lms-feature-sidebar-item/lms-feature-sidebar-item.interface';
+export * from './lib/lms-ui-sidebar/lms-ui-sidebar/lms-ui-sidebar.component';
+export * from './lib/lms-ui-sidebar/lms-ui-sidebar-item/lms-ui-sidebar-item.component';
