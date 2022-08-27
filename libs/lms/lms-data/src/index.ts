6d3b6ae2c48e65688e18a46df5bfc34ff81cef00
Aaron Chesley <FlashBanistan66@gmail.com>
2022-08-26 21:31:48 -0600

    remove old reference

--- libs/lms/lms-data/src/index.ts
+++ libs/lms/lms-data/src/index.ts
@@ -2,4 +2,3 @@ export * from './lib/authentication/lms-authentication.service';
 export * from './lib/authentication/lms-authentication.models';
 export * from './lib/authentication/jwt.service';
 export * from './lib/tag/lms-tag.service';
-export * from './lib/api/paginated.interface';
