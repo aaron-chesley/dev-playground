b86c9e662bbfc39aca6e39b4d6c601d228dc20fb
Aaron Chesley <FlashBanistan66@gmail.com>
2022-08-29 15:02:34 -0600

    remove storybook project

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 process.env.NODE_ENV = 'development';
 
 module.exports = {
-  stories: [],
+  stories: ['../**/*.stories.@(js|mdx)'],
   addons: ['@storybook/addon-essentials'],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
