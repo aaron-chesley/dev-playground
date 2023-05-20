605831f4b3aa8afb6579f8f094c51c9f6af1bec3
Aaron Chesley <FlashBanistan66@gmail.com>
2023-05-19 22:54:08 -0600

    run migrations

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,8 @@
 process.env.NODE_ENV = 'development';
-
 const path = require('path');
-
 module.exports = {
   stories: ['../**/*.stories.@(js|mdx)'],
-
-  staticDirs: [
-    path.resolve(__dirname, '../libs/cardly/cardly-ui/src/lib/shared/assets'),
-  ],
+  staticDirs: [path.resolve(__dirname, '../libs/cardly/cardly-ui/src/lib/shared/assets')]
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
@@ -15,4 +10,14 @@ module.exports = {
   //   // Return the altered config
   //   return config;
   // },
+  ,
+
+  framework: {
+    name: '@storybook/angular',
+    options: {}
+  },
+  addons: ['@storybook/addon-mdx-gfm'],
+  docs: {
+    autodocs: true
+  }
 };
--- apps/kalshi/src/main.ts
+++ apps/kalshi/src/main.ts
@@ -1,16 +1,7 @@
+import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
-import { provideRouter } from '@angular/router';
-import { APP_ROUTES } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideStore({}),
-    provideStoreDevtools(),
-    provideRouter(APP_ROUTES),
-    provideHttpClient(),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
