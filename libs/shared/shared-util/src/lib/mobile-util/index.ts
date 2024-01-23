4d1bb4c9be53ac11cff4ab583e003be0651b0d9d
Aaron Chesley <FlashBanistan66@gmail.com>
2024-01-22 19:20:05 -0700

    Add library export for mobile utilities

---
+++ libs/shared/shared-util/src/lib/mobile-util/index.ts
@@ -0,0 +1,1 @@
+export * from './request-wake-lock.function';
---
+++ libs/shared/shared-util/src/lib/mobile-util/request-wake-lock.function.ts
@@ -0,0 +1,15 @@
+export const requestWakeLock = async () => {
+  // if ('wakeLock' in navigator) {
+  //   try {
+  //     const wakeLock = await navigator.wakeLock.request('screen');
+  //     wakeLock.addEventListener('release', () => {
+  //       console.log('Screen Wake Lock released:', wakeLock.released);
+  //     });
+  //     console.log('Screen Wake Lock released:', wakeLock.released);
+  //   } catch (err: any) {
+  //     console.error(`${err.name}, ${err.message}`);
+  //   }
+  // } else {
+  //   console.warn('Wake Lock API not supported.');
+  // }
+};
