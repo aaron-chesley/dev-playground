21ff291cb73be591bf5b70839bd2f317e4f825fa
Aaron Chesley <FlashBanistan66@gmail.com>
2024-07-26 15:48:00 -0600

    Enable service worker in cardly app

--- apps/cardly/src/app/app.config.ts
+++ apps/cardly/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { appRoutes } from './app.routes';
 import { JwtModule } from '@auth0/angular-jwt';
 import { PlayModalModule } from '@playground/play-ui';
 
@@ -13,5 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     importProvidersFrom(JwtModule.forRoot({})),
     importProvidersFrom(PlayModalModule),
+    importProvidersFrom(ServiceWorkerModule.register('/ngsw-worker.js', { enabled: true })),
   ],
 };
